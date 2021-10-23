a300e8fa5d52f90547edad1b80f34abec7c943a0
naufaladrna08 <naufaladrn90@gmail.com>
2021-10-24 00:31:11 +0700

    Update

--- TP.c
+++ TP.c
@@ -2,13 +2,21 @@
 
 int main(int argc, char const *argv[]) {
   int n1, n2, i;
-  int nemu = 0;
-
+  int total1 = 0, total2 = 0;
+  
   scanf("%d", &n1);
   char x[n1];
 
   for (int i = 0; i < n1; i++) {
     scanf(" %c", &x[i]);
+
+    int r = (int) x[i] - 64;
+    if (r > 9 && r < 18)
+      r = r - 9;
+    else if (r > 18 && r < 27)
+      r = r - 18;
+
+    total1 += r;
   }
 
   scanf("%d", &n2);
@@ -16,26 +24,15 @@ int main(int argc, char const *argv[]) {
 
   for (int i = 0; i < n2; i++) {
     scanf(" %d", &y[i]);
+    total2 += y[i];
   }
 
   if (n1 == n2) {
     for (int i = 0; i < n1; i++) {
-      int res = (int) x[i] - 64;
-    
-      if (res > 9) {
-        res = res - 9;
-      }
-
-      if (res == y[0]) {
-        nemu++;
-      }
-    }
-
-    for (int i = 0; i < n1; i++) {
       printf("%c %d ", x[i], y[i]);
     }
 
-    if (nemu == 1) {
+    if (total1 == total2) {
       printf("\nNilai kartu mereka sama.\n");
       printf("Ada rasa suka di antara mereka.\n");
     } else {
